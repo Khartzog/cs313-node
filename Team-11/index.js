3bca700a1e7e5f0bab20b14cf7f7eead59d8bad4
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-02 16:15:06 -0600

    ya

--- Team-11/index.js
+++ Team-11/index.js
@@ -1,10 +1,10 @@
-function doSearch(){
- 
-    
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-//     fetch(baseUrl)
-// .then (result => {
-//     result.json()
-//     .then()
-// })
-}
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
