b01a831e7b630825879cb6faf8d21dd36a789430
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-14 22:11:25 -0600

    a

--- Assignment-08/index.js
+++ Assignment-08/index.js
@@ -1,17 +1,19 @@
+const express = require('express')
+const path = require('path')
 var http = require('http');
 var url  = require('url');
 
 http.createServer(function (req, res) {
-    if (req.url == '/home'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Welcome to the Home Page</h1>');
-    }
-    else if (req.url == '/getData'){
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end('Name: Kaylee Hartzog, Class: CS313'); 
-    }
-    else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('Page Not Found');
-    }
-  }).listen(8888);
+  if (req.url == '/home'){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Welcome to the Home Page</h1>');
+  }
+  else if (req.url == '/getData'){
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end('Name: Kaylee Hartzog, Class: CS313'); 
+  }
+  else {
+  res.writeHead(404, {'Content-Type': 'text/plain'});
+  res.end('Page Not Found');
+  }
+}).listen(8888);
